935eb47b505daa05525277a679e4247fc0c5ff51
Ricardo Iurassek <143125549+iurassek@users.noreply.github.com>
2024-02-15 15:52:45 -0300

    Update printString.cpp

--- Lesson_2_C++_Checkpoint/challenges/challenge2/printString.cpp
+++ Lesson_2_C++_Checkpoint/challenges/challenge2/printString.cpp
@@ -6,3 +6,12 @@ void printString (std::string str, int n) {
     // your code goes here! print str n times. Follow each str with a newline
     // eg. std::cout << str << std::endl;
 }
+#include "printString.h"
+#include <iostream>
+#include <string>
+
+void printString(std::string str, int n) {
+    for (int i = 0; i < n; ++i) {
+        std::cout << str << std::endl;
+    }
+}
