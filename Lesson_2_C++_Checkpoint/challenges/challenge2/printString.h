794ca3c5dd5bdbdd04dd5a0efb9e1551781915bd
Ricardo Iurassek <143125549+iurassek@users.noreply.github.com>
2024-02-15 15:54:10 -0300

    Update printString.h

--- Lesson_2_C++_Checkpoint/challenges/challenge2/printString.h
+++ Lesson_2_C++_Checkpoint/challenges/challenge2/printString.h
@@ -10,3 +10,15 @@ This header file defines the function signature for PrintString.
 void printString (std::string, int);
 
 #endif // PRINTSTRING_H
+/*
+This header file defines the function signature for PrintString.
+*/
+
+#ifndef PRINTSTRING_H
+#define PRINTSTRING_H
+
+#include <string>
+
+void printString(std::string, int);
+
+#endif // PRINTSTRING_H
