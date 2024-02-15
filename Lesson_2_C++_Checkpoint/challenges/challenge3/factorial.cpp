0a7d10c0cc5eb7fbeae66448fcc97f5c6c922392
Ricardo Iurassek <143125549+iurassek@users.noreply.github.com>
2024-02-15 15:54:58 -0300

    Update factorial.cpp

--- Lesson_2_C++_Checkpoint/challenges/challenge3/factorial.cpp
+++ Lesson_2_C++_Checkpoint/challenges/challenge3/factorial.cpp
@@ -3,3 +3,13 @@
 // Your code goes here
 // See factorial.h for your method's signature 
 // (eg. its args and return value)
+#include "factorial.h"
+
+// Function definition for factorial
+int factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
