5094a4789c91c62e80c6c506ae4dee39e6424e64
Ricardo Iurassek <143125549+iurassek@users.noreply.github.com>
2024-02-15 16:26:45 -0300

    Update sm1-main.cpp

--- Lesson_6_Utilizing_Scan_Matching/Exercise-ICP-Alignment/sm1-main.cpp
+++ Lesson_6_Utilizing_Scan_Matching/Exercise-ICP-Alignment/sm1-main.cpp
@@ -3,6 +3,7 @@
 // Aaron Brown
 
 using namespace std;
+
 #include <string>
 #include <pcl/io/pcd_io.h>
 #include <pcl/visualization/pcl_visualizer.h>
@@ -13,15 +14,18 @@ using namespace std;
 #include <ctime> 
 #include <pcl/registration/icp.h>
 #include <pcl/registration/ndt.h>
-#include <pcl/console/time.h>
+#include <pcl/console/time.h>   // TicToc
 
-enum Registration{ Off, Icp};
+
+enum Registration{ Off, Icp, Ndt};
 Registration matching = Off;
 
 Pose pose(Point(0,0,0), Rotate(0,0,0));
 Pose savedPose = pose;
 void keyboardEventOccurred(const pcl::visualization::KeyboardEvent &event, void* viewer)
 {
+
+  	//boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer = *static_cast<boost::shared_ptr<pcl::visualization::PCLVisualizer> *>(viewer_void);
 	if (event.getKeySym() == "Right" && event.keyDown()){
 		matching = Off;
 		pose.position.x += 0.1;
@@ -53,6 +57,9 @@ void keyboardEventOccurred(const pcl::visualization::KeyboardEvent &event, void*
 	else if(event.getKeySym() == "i" && event.keyDown()){
 		matching = Icp;
 	}
+	else if(event.getKeySym() == "n" && event.keyDown()){
+		matching = Ndt;
+	}
 	else if(event.getKeySym() == "space" && event.keyDown()){
 		matching = Off;
 		pose = savedPose;
@@ -66,11 +73,52 @@ void keyboardEventOccurred(const pcl::visualization::KeyboardEvent &event, void*
 
 Eigen::Matrix4d ICP(PointCloudT::Ptr target, PointCloudT::Ptr source, Pose startingPose, int iterations){
 
+	// Defining a rotation matrix and translation vector
   	Eigen::Matrix4d transformation_matrix = Eigen::Matrix4d::Identity ();
 
-  	// TODO: Implement the PCL ICP function and return the correct transformation matrix
-  	// .....
-  	
+  	// align source with starting pose
+  	Eigen::Matrix4d initTransform = transform3D(startingPose.rotation.yaw, startingPose.rotation.pitch, startingPose.rotation.roll, startingPose.position.x, startingPose.position.y, startingPose.position.z);
+  	PointCloudT::Ptr transformSource (new PointCloudT); 
+  	pcl::transformPointCloud (*source, *transformSource, initTransform);
+
+  	/*
+  	if( count == 0)
+  		renderPointCloud(viewer, transformSource, "transform_scan_"+to_string(count), Color(1,0,1)); // render corrected scan
+  	*/
+	
+	pcl::console::TicToc time;
+  	time.tic ();
+  	pcl::IterativeClosestPoint<PointT, PointT> icp;
+  	icp.setMaximumIterations (iterations);
+  	icp.setInputSource (transformSource);
+  	icp.setInputTarget (target);
+	icp.setMaxCorrespondenceDistance (2);
+	//icp.setTransformationEpsilon(0.001);
+	//icp.setEuclideanFitnessEpsilon(.05);
+	//icp.setRANSACOutlierRejectionThreshold (10);
+
+  	PointCloudT::Ptr cloud_icp (new PointCloudT);  // ICP output point cloud
+  	icp.align (*cloud_icp);
+  	//std::cout << "Applied " << iterations << " ICP iteration(s) in " << time.toc () << " ms" << std::endl;
+
+  	if (icp.hasConverged ())
+  	{
+  		//std::cout << "\nICP has converged, score is " << icp.getFitnessScore () << std::endl;
+  		transformation_matrix = icp.getFinalTransformation ().cast<double>();
+  		transformation_matrix =  transformation_matrix * initTransform;
+  		//print4x4Matrix(transformation_matrix);
+
+
+  		/*
+  		PointCloudT::Ptr corrected_scan (new PointCloudT);
+  		pcl::transformPointCloud (*source, *corrected_scan, transformation_matrix);
+  		if( count == 1)
+  			renderPointCloud(viewer, corrected_scan, "corrected_scan_"+to_string(count), Color(0,1,1)); // render corrected scan
+		*/
+  		return transformation_matrix;
+  	}
+	else
+  		cout << "WARNING: ICP did not converge" << endl;
   	return transformation_matrix;
 
 }
@@ -84,6 +132,15 @@ void drawCar(Pose pose, int num, Color color, double alpha, pcl::visualization::
     box.cube_width = 2;
     box.cube_height = 2;
 	renderBox(viewer, box, num, color, alpha);
+}
+
+void loadScans(vector<PointCloudT::Ptr>& scans, int num){
+	for(int index = 0; index < num; index++){
+		// Load scan
+		PointCloudT::Ptr scanCloud(new PointCloudT);
+  		pcl::io::loadPCDFile("scan"+to_string(index+1)+".pcd", *scanCloud);
+  		scans.push_back(scanCloud);
+	}
 }
 
 struct Tester{
@@ -150,25 +207,30 @@ int main(){
 
 	// Load map and display it
 	PointCloudT::Ptr mapCloud(new PointCloudT);
-  	pcl::io::loadPCDFile("map.pcd", *mapCloud);
+  	if (pcl::io::loadPCDFile("map.pcd", *mapCloud) == -1) //* load the file
+  	{
+    	PCL_ERROR ("Couldn't read file \n");
+  	}
   	cout << "Loaded " << mapCloud->points.size() << " data points from map.pcd" << endl;
+	
 	renderPointCloud(viewer, mapCloud, "map", Color(0,0,1)); 
 
-	// True pose for the input scan
 	vector<Pose> truePose ={Pose(Point(2.62296,0.0384164,0), Rotate(6.10189e-06,0,0)), Pose(Point(4.91308,0.0732088,0), Rotate(3.16001e-05,0,0))};
+
+	vector<PointCloudT::Ptr> scans;
+	loadScans(scans, 1);
+
 	drawCar(truePose[0], 0,  Color(1,0,0), 0.7, viewer);
-
-	// Load input scan
-	PointCloudT::Ptr scanCloud(new PointCloudT);
-  	pcl::io::loadPCDFile("scan1.pcd", *scanCloud);
-
+	
+	pcl::VoxelGrid<PointT> vg;
+  	vg.setInputCloud(scans[0]);
+	double filterRes = 0.5;
+  	vg.setLeafSize(filterRes, filterRes, filterRes);
 	typename pcl::PointCloud<PointT>::Ptr cloudFiltered (new pcl::PointCloud<PointT>);
-
-	cloudFiltered = scanCloud; // TODO: remove this line
-	//TODO: Create voxel filter for input scan and save to cloudFiltered
-	// ......
+  	vg.filter(*cloudFiltered);
 
 	PointCloudT::Ptr transformed_scan (new PointCloudT);
+
 	Tester tester;
 
 	while (!viewer->wasStopped())
@@ -176,8 +238,8 @@ int main(){
 		Eigen::Matrix4d transform = transform3D(pose.rotation.yaw, pose.rotation.pitch, pose.rotation.roll, pose.position.x, pose.position.y, pose.position.z);
 
 		if( matching != Off){
-			if( matching == Icp)
-				transform = ICP(mapCloud, cloudFiltered, pose, 0); //TODO: change the number of iterations to positive number
+			if(matching == Icp)
+				transform = ICP(mapCloud, cloudFiltered, pose, 3);
   			pose = getPose(transform);
 			if( !tester.Displacement(pose) ){
 				if(matching == Icp)
