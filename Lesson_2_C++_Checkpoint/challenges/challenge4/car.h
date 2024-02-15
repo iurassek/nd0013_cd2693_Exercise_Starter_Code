ebd4418248e0087df447515a2dac3d1c92dd19c5
Ricardo Iurassek <143125549+iurassek@users.noreply.github.com>
2024-02-15 15:59:58 -0300

    Update car.h

--- Lesson_2_C++_Checkpoint/challenges/challenge4/car.h
+++ Lesson_2_C++_Checkpoint/challenges/challenge4/car.h
@@ -7,4 +7,17 @@ Hint: you'll need to define:
 3. one private property
 4. three public methods
 */
+#ifndef CAR_H
+#define CAR_H
 
+class Car {    
+    public:
+        Car();
+        void wearAndTear();
+        bool drive();
+        void fix();
+    private:
+        bool in_working_condition_;
+};
+
+#endif  // CAR_H
