30ecbb8e7b3f8afd65b949a3f8ce25f814ef4aaf
Ricardo Iurassek <143125549+iurassek@users.noreply.github.com>
2024-02-15 15:51:50 -0300

    Update main.cpp

--- Lesson_2_C++_Checkpoint/solutions/solution2/main.cpp
+++ Lesson_2_C++_Checkpoint/solutions/solution2/main.cpp
@@ -8,3 +8,10 @@ No need to change this file (unless you want to change the test case below)
 int main() {
     printString ("This is a test.", 10);
 }
+#include <iostream>
+#include "printString.h"
+
+int main() {
+    printString("This is a test.", 10);
+    return 0;
+}
