943bb6752cf697e03e39769737897990b8617eee
Ricardo Iurassek <143125549+iurassek@users.noreply.github.com>
2024-02-15 15:49:23 -0300

    Update main.cpp

--- Lesson_2_C++_Checkpoint/solutions/solution1/main.cpp
+++ Lesson_2_C++_Checkpoint/solutions/solution1/main.cpp
@@ -4,3 +4,11 @@ int main() {
   std::cout << "no more steering wheels" << std::endl;
   return 0;
 }
+
+#include <iostream>
+
+int main() {
+  // write your code here!
+  // print "no more steering wheels" to stdout
+  return 0;
+}
